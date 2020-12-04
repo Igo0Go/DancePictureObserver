ecf40b0df5981536db06054b287afcd99dc339f2
Igo_Go <chudakin98@gmail.com>
2020-12-04 15:43:40 +0700

    Добавление скрипта для работы с файлами

---
+++ DancePictureObserverProj/Assets/Scripts/FileModule/FileReaderModule.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class FileReaderModule : MonoBehaviour
+{
+    private string saveFilesDirectory = Path.Combine(Application.streamingAssetsPath, "Saves");
+
+    public GameObject saveFilePrefab;
+    public Transform filesPanel;
+
+    private void Start()
+    {
+        Debug.Log(saveFilesDirectory);
+    }
+}
